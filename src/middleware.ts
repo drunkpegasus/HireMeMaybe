9b0944d3ee5f7195277e8454761fe126ed82cb96
Kaustubh Shastrakar <kaustubhshastrakar01@gmail.com>
2025-11-01 00:22:57 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { type NextRequest, NextResponse } from "next/server";
 // This middleware no longer logs.
 // It just passes the request to the next step.
 // All logging is now handled by _app.tsx.
+// eslint-disable-next-line no-unused-vars
 export function middleware(_request: NextRequest) {
   return NextResponse.next();
 }
