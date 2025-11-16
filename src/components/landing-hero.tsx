228c4611391f8855222bd1a68d56f3ff8b32ebf7
Kaustubh Shastrakar <kaustubhshastrakar01@gmail.com>
2025-11-17 00:18:42 +0530

    visual fixes

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -41,9 +41,13 @@ export default function LandingHero() {
               <h1 className="bg-accent bg-clip-text py-2 text-5xl font-bold text-transparent sm:text-6xl md:text-7xl xl:text-8xl">
                 Kaustubh
               </h1>
-              <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
+              {/* <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
                 Software Developer
-              </span>
+              </span> */}
+
+              <div className="mt-1">
+                <div className="h-[1px] w-52 bg-accent/60"></div>
+              </div>
             </FadeUp>
             <FadeUp key="description" duration={0.6} delay={0.2}>
               <div className="mt-8 max-w-3xl text-base font-semibold text-zinc-900 dark:text-zinc-200 sm:text-base md:text-xl">
