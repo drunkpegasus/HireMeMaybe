228465fabeee2a2edc158a3576a6ad716abb0e53
Kaustubh Shastrakar <kaustubhshastrakar01@gmail.com>
2025-12-04 19:20:35 +0530

    favicon updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+      </Head>
       <body className="bg-background text-zinc-950 antialiased selection:bg-accent selection:text-background">
         <Main />
         <NextScript />
